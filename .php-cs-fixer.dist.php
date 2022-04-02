33350770a15408326e4fcc360f116c8a636df3be
Yusuf Kandemir <yusuf.kandemir@outlook.com.tr>
2022-04-02 03:59:08 +0300

    chore: Migrate from PHP Codesniffer to PHP CS Fixer and configure it

---
+++ .php-cs-fixer.dist.php
@@ -0,0 +1,20 @@
+<?php
+
+$finder = Symfony\Component\Finder\Finder::create()
+    ->in([
+        __DIR__ . '/src',
+        __DIR__ . '/tests',
+    ])
+    ->name('*.php')
+    ->ignoreDotFiles(true)
+    ->ignoreVCS(true);
+
+return (new PhpCsFixer\Config())
+    ->setRules([
+        '@Symfony' => true,
+        '@Symfony:risky' => true,
+        'concat_space' => ['spacing' => 'one'],
+        'yoda_style' => ['equal' => false, 'identical' => false, 'less_and_greater' => false],
+    ])
+    ->setRiskyAllowed(true)
+    ->setFinder($finder);
